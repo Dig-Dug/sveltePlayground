83ace74fa191b89ce3acdf78fd9e2903657a025f
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-06-30 12:08:54 +0200

    -

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,45 @@
 
 import Events from './lib/events.svelte';
 import Binding from './lib/binding.svelte';
-
+import Keypad from './lib//Keypad.svelte';
 
 //Component Events
 function handleMsg(event) {
 		alert(event.detail.text);
 	}
+//ComponentBindings
+let pin;
+	$: view = pin ? pin.replace(/\d(?!$)/g, '•') : 'enter your pin';
+
+	function handleSubmit() {
+		alert(`submitted ${pin}`);
+	}
 
 </script>
 
 <!-- Component Events -->
-<Events on:message={handleMsg}/>
-<Binding/>
+<div class="thre">
+<div><Events on:message={handleMsg}/></div>	
+	<div><Binding/></div>
+	
+	<div><h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
+	
+		<Keypad bind:value={pin} on:submit={handleSubmit}/></div>
+	
+</div>
+
+<style>
+	.thre {  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-auto-columns: 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    ". . ."
+    ". . ."
+    ". . .";
+}
+
+</style>
+
--- src/lib/binding.svelte
+++ src/lib/binding.svelte
@@ -1,30 +1,43 @@
 <script>
-    
-    let input = "wold"; //ONE
-
-    let a = 2; let b = 2; //TWO
-
-    let  yes = false;  //THREE
 
 
+let input = "wold"; //ONE
+let a = 2; let b = 2; //TWO
+let  yes = false;  //THREE
 //FOUR
  let questions = [
 		{ id: 1, text: `Where did you go to school?` },
 		{ id: 2, text: `What is your mother's name?` },
 		{ id: 3, text: `What is another personal fact that an attacker could easily find with Google?` }
 	];
+let selected; let answer = '';
 
-	let selected;
-
-	let answer = '';
-
-	function handleSubmit() {
-		alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`);
+function handleSubmit() {
+		alert(`answered question ${selected.id} 
+        (${selected.text}) with "${answer}"`);
 	}
+////////////////
+let html = '<p>Write some text!</p>';
+/////////////////
 
-
-
-
+let some =[
+   		{ done: false, txt: 'finish Svelte tutorial' },
+		{ done: false, txt: 'build an app' },
+		{ done: false, txt: 'world domination' }
+    ]
+function add(){
+    some = some.concat({done: false, txt: ''});
+}
+function clear(){
+    some = some.filter(t => !t.done);
+}
+$: remaining = some.filter(t => !t.done).length;
+////////////////////////////////
+//AUDIO VIDEO https://svelte.dev/tutorial/media-elements
+//////////////////////////////////
+let h;
+let w;
+let size = 42, text ="editMoi";
 
 </script>
 
@@ -43,7 +56,6 @@
  <p>{a} + {b} = {a + b}</p> 
 -->
 
-
 <!--THREE <label>
     <input type=checkbox bind:checked={yes}>
     Yes! Send me regular email spam
@@ -55,14 +67,61 @@
     {/if}
  <button disabled={!yes}>Subscribe</button> 
 -->
+<!--Select Bindings-->
+<form on:submit|preventDefault={handleSubmit}>
+	<select bind:value={selected} on:change="{() => answer = ''}">
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
+<input bind:value={answer}>
+<button disabled={!answer} type=submit>
+		Submit
+	</button>
+</form>
+<!--//////////////////////////////-->
+<div contenteditable="true" bind:innerHTML={html}></div>
+<pre>{html}</pre>
+<!--//////////////////////////////-->
+<h1>TODO &#9924 </h1>
+{#each some as somes}
+    <div class:done={somes.done} id="done">
+    <input type=checkbox bind:checked={somes.done}>
+    <input placeholder="you do wot?" bind:value={somes.txt}>
+    </div>
+{/each}
+<p>{remaining}---Remaining</p> 
+<button on:click={add}>Add</button>
+<button on:click={clear}>Delete</button>
+<!--//////////////////////////////-->
+<input type=range bind:value={size}>
+<input bind:value={text}>
+
+<p>size: {w}px x {h}px</p>
+
+<div bind:clientWidth={w} bind:clientHeight={h}>
+	<span style="font-size: {size}px">{text}</span>
+</div>
+<!--//////////////////////////////-->
+
 
 
 
 <!--for too many =>  https://svelte.dev/tutorial/group-inputs-->
-<!--can#t get it =>  https://svelte.dev/tutorial/textarea-inputs-->
+<!--big file :( https://svelte.dev/tutorial/multiple-select-bindings-->
+<!--https://svelte.dev/tutorial/bind-this-->
+
 
 
 <style>
+    #done{
+        display: grid; 
+  grid-auto-columns: 1fr; 
+  grid-template-rows: 1fr 1fr 1fr; 
+  gap: 0px 0px; 
+    }
 .in{
     position: relative;
     top: 27px;
@@ -70,5 +129,24 @@
 
 }
 label { display: flex }
-input, p { margin: 6px }
+input, p { margin: 6px; margin-bottom: 0px,}
+.done {opacity: 0.4;}
+
+input {
+		display: block;
+		width: 500px;
+		max-width: 100%;
+
+        
+	}
+[contenteditable] {
+		padding: 0.5em;
+		border: 1px solid #eee;
+		border-radius: 4px;
+}
+pre{
+    margin-top: 0px;
+}
+div { display: inline-block; }
+	span { word-break: break-all; }
 </style>
